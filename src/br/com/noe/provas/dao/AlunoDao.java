d2754acaf85391387216b48946b8abcff8660041
Nilton <Nilton@Nilton-PC>
2015-09-19 10:27:19 -0300

    Models e interface criadas

---
+++ src/br/com/noe/provas/dao/AlunoDao.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.noe.provas.dao;
+
+import br.com.noe.provas.model.Aluno;
+import java.util.List;
+
+/**
+ *
+ * @author Nilton
+ */
+public class AlunoDao implements IAlunoDao{
+
+    @Override
+    public void cadastrarAluno(Aluno aluno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Aluno buscarAluno(Long id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizarAluno(Aluno aluno) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletarAluno(Aluno aluno) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Aluno> listarAlunos() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
