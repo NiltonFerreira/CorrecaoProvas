c1dd5a8cd86b5866553993712e61f1141a0aa772
Nilton <Nilton@Nilton-PC>
2015-09-19 10:39:23 -0300

    Model endereco criado

---
+++ src/br/com/noe/provas/dao/ProfessorDao.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.noe.provas.dao;
+
+import br.com.noe.provas.model.Professor;
+import java.util.List;
+
+/**
+ *
+ * @author Nilton
+ */
+public class ProfessorDao implements IProfessorDao{
+
+    @Override
+    public void cadastrarProfessor(Professor professor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Professor buscarProfessor(Long id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizarProfessor(Professor professor) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletarProfessor(Professor professor) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Professor> listarProfessor() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
