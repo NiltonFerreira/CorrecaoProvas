f72ac60ae3ce9cbfa12fef17ecf6d8e8ebc560e5
Nilton <Nilton@Nilton-PC>
2015-09-22 02:17:08 -0300

    Daos Revisados falta revisar ProfessorDao

--- src/br/com/noe/provas/test/CorreçãoProvas.java
+++ src/br/com/noe/provas/test/CorreçãoProvas.java
@@ -6,8 +6,12 @@
 package br.com.noe.provas.test;
 
 import br.com.noe.provas.facade.Facade;
+import br.com.noe.provas.model.Aluno;
+import br.com.noe.provas.model.Endereco;
+import br.com.noe.provas.model.Professor;
 import br.com.noe.provas.model.Turma;
 import static java.time.Clock.system;
+import java.util.List;
 
 /**
  *
@@ -19,23 +23,43 @@ public class CorreçãoProvas {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        Facade face = new Facade();
+        Facade facade = new Facade();
+        //String cidade, String bairro, String rua, String cep
+        Endereco endereco = new Endereco("Patos", "Belo Horizonte", "Horacio nobrega", "58704000");
+        //String nome, String cpf, Endereco endereco
+        Professor professor1 = new Professor("Juca", "10318863580", endereco);
+        Professor professor2 = new Professor("Jandira", "21429974691", endereco);
+        Professor professor3 = new Professor("Jaco", "32531195602", endereco);
+        //String nome, String cpf, Endereco endereco
+        Aluno aluno1 = new Aluno("Nilton", "10419972480", endereco);
+        Aluno aluno2 = new Aluno("Otto", "20419972480", endereco);
+        Aluno aluno3 = new Aluno("Ellan", "30419972480", endereco);
+        //String nome, int ano
+        Turma turma = new Turma("A", 2015);
         
-        Turma turma = new Turma("A",2015);
-        Turma turma2 = new Turma("B",2015);
-        Turma turma3 = new Turma("C",2015);
-        Turma turma4 = new Turma("D",2015);
-        Turma turma5 = new Turma("E",2015);
-        Turma turma6 = new Turma("F",2015);
+        /*facade.cadastrarAluno(aluno1);
+        facade.cadastrarAluno(aluno2);
+        facade.cadastrarAluno(aluno3);
         
-        face.cadastrarTurma(turma);
-        face.cadastrarTurma(turma2);
-        face.cadastrarTurma(turma3);
-        face.cadastrarTurma(turma4);
-        face.cadastrarTurma(turma5);
-        face.cadastrarTurma(turma6);
+        facade.cadastrarProfessor(professor1);
+        facade.cadastrarProfessor(professor2);
+        facade.cadastrarProfessor(professor3);
         
-        System.out.println(face.listarTurmas());
+        facade.cadastrarTurma(turma);*/
+        aluno1 = facade.buscarAluno(4l);
+        aluno2 = facade.buscarAluno(5l);
+        aluno3 = facade.buscarAluno(6l);
+
+        //facade.alocarProfessor(turma, professor1);
+        //facade.alocarProfessor(turma, professor2);
+        //facade.alocarProfessor(turma, professor3);
+        turma = facade.buscarTurma(2l);
+        System.out.println(aluno1);
+        System.out.println(aluno2);
+        System.out.println(aluno3);
+        //facade.matricularAluno(turma, aluno1);
+        //facade.matricularAluno(turma, aluno2);
+        //facade.matricularAluno(turma, aluno3);
     }
 
 }
