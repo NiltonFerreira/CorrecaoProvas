9b98ca60831834d8fbbfe8b50a0f5630507e9b8d
Nilton <Nilton@Nilton-PC>
2015-09-22 19:31:46 -0300

    Todo projeto revisado e funcionando

--- src/br/com/noe/provas/test/CorreçãoProvas.java
+++ src/br/com/noe/provas/test/CorreçãoProvas.java
@@ -10,7 +10,7 @@ import br.com.noe.provas.model.Aluno;
 import br.com.noe.provas.model.Endereco;
 import br.com.noe.provas.model.Professor;
 import br.com.noe.provas.model.Turma;
-
+import static java.time.Clock.system;
 import java.util.List;
 
 /**
@@ -31,32 +31,48 @@ public class CorreçãoProvas {
         Professor professor2 = new Professor("Jandira", "21429974691", endereco);
         Professor professor3 = new Professor("Jaco", "32531195602", endereco);
         //String nome, String cpf, Endereco endereco
-        Aluno aluno1 = new Aluno("Nilton", "10419972480", endereco);
-        Aluno aluno2 = new Aluno("Otto", "20419972480", endereco);
-        Aluno aluno3 = new Aluno("Ellan", "30419972480", endereco);
+        Aluno aluno1= new Aluno("Nilton", "10419972480", endereco);
+        Aluno aluno2= new Aluno("Otto", "20419972480", endereco);
+        Aluno aluno3= new Aluno("Ellan", "30419972480", endereco);
         //String nome, int ano
-        Turma turma = new Turma("A", 2015);
+        Turma turma= new Turma("A", 2015);
         
-        /*facade.cadastrarAluno(aluno1);
-        facade.cadastrarAluno(aluno2);
-        facade.cadastrarAluno(aluno3);
+        //facade.cadastrarTurma(turma);
         
-        facade.cadastrarProfessor(professor1);
-        facade.cadastrarProfessor(professor2);
-        facade.cadastrarProfessor(professor3);
+        //facade.cadastrarAluno(aluno1);
+        //facade.cadastrarAluno(aluno2);
+        //facade.cadastrarAluno(aluno3);
         
-        facade.cadastrarTurma(turma);*/
-        aluno1 = facade.buscarAluno(4l);
-        aluno2 = facade.buscarAluno(5l);
-        aluno3 = facade.buscarAluno(6l);
-
-        //facade.alocarProfessor(turma, professor1);
-        //facade.alocarProfessor(turma, professor2);
-        //facade.alocarProfessor(turma, professor3);
-        turma = facade.buscarTurma(2l);
-        System.out.println(aluno1);
-        System.out.println(aluno2);
-        System.out.println(aluno3);
+        //facade.cadastrarProfessor(professor1);
+        //facade.cadastrarProfessor(professor2);
+        //facade.cadastrarProfessor(professor3);
+        
+        //facade.matricularAluno(turma, aluno1);
+        //facade.matricularAluno(turma, aluno2);
+        //facade.matricularAluno(turma, aluno3);
+        
+        //aluno1 = facade.buscarAluno(4l);
+        //aluno2 = facade.buscarAluno(5l);
+        //aluno3 = facade.buscarAluno(6l);
+        professor1 = facade.buscarProfessor(8l);
+        professor2 = facade.buscarProfessor(9l);
+        professor3 = facade.buscarProfessor(10l);
+        System.out.println(professor1);
+        System.out.println(professor2);
+        System.out.println(professor3);
+        System.out.println(facade.buscarAluno(10l));
+        System.out.println(facade.buscarAluno(11l));
+        System.out.println(facade.buscarAluno(12l));
+        turma = facade.buscarTurma(4l);
+        System.out.println(turma);
+        facade.alocarProfessor(turma, professor1);
+        facade.alocarProfessor(turma, professor2);
+        facade.alocarProfessor(turma, professor3);
+        
+        //facade.fecharTurma(turma);
+        //System.out.println(aluno1);
+        //System.out.println(aluno2);
+        //System.out.println(aluno3);
         //facade.matricularAluno(turma, aluno1);
         //facade.matricularAluno(turma, aluno2);
         //facade.matricularAluno(turma, aluno3);
